4789d669b673b29734b410314acd9f55f36900c0
AshvinJoseph2 <ashvinj893@gmail.com>
2025-11-20 21:08:27 +0530

    initial commit

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Preloader() {
+  return (
+    <div className='flex justify-center items-center w-full h-screen '>
+      <img width={'200px'} src="https://cdn-icons-gif.flaticon.com/12743/12743736.gif" alt="" />
+    </div>
+  )
+}
+
+export default Preloader
---
+++ src/user/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaInstagram, FaUser } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+function Header() {
+  return (
+    <div className='grid grid-cols-3 p-3'>
+      {/* logo */}
+      <div className='flex items-center'>
+        <img width={'50px'} height={'50px'} src="https://clipground.com/images/book-icon-clipart-3.png" alt="logo" />
+        <h1 className='text-2xl font-bold ms-2 md:hidden'>BOOKSTORE</h1>
+      </div>
+      {/* title */}
+      <div className='md:flex justify-center items-center hidden'>
+        <h1 className='text-3xl font-bold'>BOOK STORE</h1>
+      </div>
+      {/* login */}
+      <div className='md:flex justify-end items-center hidden'>
+        <FaInstagram className='me-1'/>
+        <FaXTwitter className='me-1'/>
+        <FaFacebookF/>
+        <Link to={'/login'} className='border border-black rounded px-3 py-2 ms-3 flex items-center hover:bg-black hover:text-white'><FaUser className='me-1'/>Login</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header
